51d23e489ecb126e33fcf80b1b6b039b6a82ad62
Denis <neothat@mail.ru>
2022-02-27 14:00:28 +0300

    Added back movement and created an asteroid template

---
+++ core/src/ru/geekbrains/stargame/Asteroid.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.stargame;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Asteroid {
+
+    private Texture textureAsteroid;
+    private Vector2 position;
+    private Vector2 velocity;
+    private float angle;
+
+    public Asteroid() {
+        this.textureAsteroid = new Texture("asteroid.png");
+        this.position = new Vector2(ScreenManager.SCREEN_WIDTH + 256, 360);
+        this.velocity = new Vector2(0, 0);
+        this.angle = 0.0f;
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(textureAsteroid, position.x - 128, position.y - 128, 128, 128,
+                256, 256, 0.5f, 0.5f, angle, 0, 0, 256, 256,
+                false, false);
+    }
+
+    public void update(float dt) {
+
+    }
+}
